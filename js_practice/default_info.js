ebde96ad28e37b547932bd193c81140eb58e1fa1
SI-Tanbir <si017490@gmail.com>
2024-11-04 03:36:43 +0600

    just added another default function

---
+++ js_practice/default_info.js
@@ -0,0 +1,12 @@
+// Write a function that formats and displays user information. If some information is missing (like age or country), use default values.
+
+function showuser(name='unknow',age="not specified",country="unknown"){
+
+    console.log(`Name :${name} , Age:${age} , Country:${country}`);
+
+
+}
+
+showuser('shafikul',22);
+
+
