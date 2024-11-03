6eb1ea8ee1d22c3ec7b60c7607540aaa590f92ec
SI-Tanbir <si017490@gmail.com>
2024-11-04 03:29:18 +0600

    adding js practice to practice the concept

---
+++ js_practice/default_name.js
@@ -0,0 +1,9 @@
+// Write a function that greets a user with a message. If no name is provided, it should use "Guest" as the default name.
+
+function greet(name='Guest'){
+
+    console.log("Welcom back",name);
+};
+
+greet();
+greet('shafikul')
