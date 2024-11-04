6467aa9d27c7e69381702d88c0c614f91e731dc4
SI-Tanbir <si017490@gmail.com>
2024-11-04 12:50:04 +0600

    practice problem of js

---
+++ js_practice/1_task.js
@@ -0,0 +1,6 @@
+// write an arrow function that will take 3 parameters ,will multiply the parameters and will return the result;
+
+let arrow = (para1, para2, para3) => para1 * para2 * para3;
+
+let output = arrow(1, 2, 3);
+console.log(output);
---
+++ js_practice/2_task.js
@@ -0,0 +1,8 @@
+// write the following sentence in there and print the result 
+// i am a web developer. i love to code. i love to eat biryani.
+
+let sen1="i am a web developer .";
+let sen2='I love to code.';
+let sen3="i love to eat biryani.";
+
+console.log(`${sen1} ${sen2} ${sen3}`)
