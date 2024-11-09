5cfcf1a1b8fccd43ae315cf0f010505d2331d4d0
SI-Tanbir <si017490@gmail.com>
2024-11-09 12:34:51 +0600

    doing some updaate

---
+++ js_practice/13_task.js
@@ -0,0 +1,16 @@
+// 1a) Write a function that displays a message after 5s.
+// // 1b) Write a function called delayedGreeting() that takes two parameters, name and delay time, and logs a greeting message after the given delay time.
+
+// // Input:
+// // delayGreeting(‘Alice’, 2000)
+
+// // Output:
+// // Hello, Alice!
+
+console.log(1)
+setTimeout(() => {
+    console.log('delayed',2);
+},2000)
+console.log(3)
+console.log(4)
+
